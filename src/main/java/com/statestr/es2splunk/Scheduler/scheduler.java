74988a510884a3b7fa96c0d3402238df50813ad3
zibu <varzibu@163.com>
2020-10-08 23:50:21 +0800

    [Add]

--- src/main/java/com/statestr/es2splunk/Scheduler/scheduler.java
+++ src/main/java/com/statestr/es2splunk/Scheduler/scheduler.java
@@ -1,6 +1,7 @@
 package com.statestr.es2splunk.Scheduler;
 
 import com.statestr.es2splunk.Utils.ESRestfulUtils;
+import org.elasticsearch.search.SearchHit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
@@ -8,6 +9,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Date;
 
 // 定时任务触发器
 @PropertySource("classpath:application.properties")
@@ -23,14 +25,36 @@ public class scheduler {
     @Scheduled(cron = "${schedule.service.interval}")
     private void scheduled(){
 
+        Date startTime = new Date(System.currentTimeMillis());
         // Get All Configuration entries from elasticsearch
+        // 从ES中获取所有的数据源索引的条目信息
         try {
-            esRestfulUtils.getConfigEntries(esMetaDataIndexName);
+            SearchHit[] esDataSourceList = esRestfulUtils.getConfigEntries(esMetaDataIndexName);
+            for (int i = 0; i < esDataSourceList.length; i++) {
+                System.out.println("Failed to get es metaData");
+                String indexName = (String) esDataSourceList[0].getSourceAsMap().get("indexName");
+                Date fromDate = (Date) esDataSourceList[0].getSourceAsMap().get("fromDate");
+                Date toDate = (Date) esDataSourceList[0].getSourceAsMap().get("toDate");
+                // 如果todate 存在，且 fromDate>=toDate则返回
+                if (false){
+                    continue;
+                }
+                // 直到 Fromdate 大于startTime || 某一批次返回的数据不足10条，表示剩下的数据已经都读出来了
+                while (true){
+                    // 访问数据  每次从ES中拿10条数据
+                    esRestfulUtils.timeRangeSearchScroll(fromDate, toDate, indexName);
+                    // 将10条数据 写入 Splunk 中
+                    // 更新ES数据源 metadata entries 的 fromDate
+                }
+
+            }
         } catch (IOException e) {
             System.out.println("Failed to get es metaData");
             e.printStackTrace();
         }
-        System.out.println("aaa");
+
     }
+
+
 
 }
--- src/main/java/com/statestr/es2splunk/Utils/ESRestfulUtils.java
+++ src/main/java/com/statestr/es2splunk/Utils/ESRestfulUtils.java
@@ -8,13 +8,10 @@ import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
-import org.apache.http.impl.bootstrap.HttpServer;
 import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.action.search.*;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.index.query.QueryBuilders;
@@ -30,7 +27,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -51,7 +47,7 @@ public class ESRestfulUtils {
             final CredentialsProvider credentialsProvider =
                     new BasicCredentialsProvider();
             credentialsProvider.setCredentials(AuthScope.ANY,
-                    new UsernamePasswordCredentials("esUser", "esPassword"));
+                    new UsernamePasswordCredentials(esUser, esPassword));
 
             client = new RestHighLevelClient(
                     RestClient.builder(
